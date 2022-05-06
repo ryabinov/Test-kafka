24b02391cc69ea3c2c45b8b00ba34ef15498b032
DRiabinov <https://github.com/>
2022-05-06 14:37:49 +0300

    fdg

--- Minio-test/src/main/java/com/example/miniotest/MinioTestApplication.java
+++ Minio-test/src/main/java/com/example/miniotest/MinioTestApplication.java
@@ -11,3 +11,5 @@ public class MinioTestApplication {
     }
 
 }
+//Console: http://172.17.0.2:9001 http://127.0.0.1:9001
+//        docker run -p 9000:9000 -p 9001:9001 --name minio1 -v C:\my_programs\data:/data -e "MINIO_ROOT_USER=admin" -e "MINIO_ROOT_PASSWORD=admin" quay.io/minio/minio server /data --console-address ":9001"
